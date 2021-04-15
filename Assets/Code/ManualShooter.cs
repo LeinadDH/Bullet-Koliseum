871ad8f33c13a8327339a3c4e706f9a3601b917f
Daniel Navarrete <daniel_navarrete_05@hotmail.com>
2021-04-15 02:14:57 -0500

    Escenario de testeo y un intento de arma

---
+++ Assets/Code/ManualShooter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManualShooter : ShooterBehaviour
+{
+    protected override IEnumerator IdleState()
+    {
+        while (true)
+        {
+            yield return null;
+
+            if (Input.GetButtonDown("Fire1"))
+            {
+                if (bulletRemaining > 0)
+                {
+                    shootingState = ShootingState.Shoot;
+                    yield return new WaitUntil(() => shootingState == ShootingState.Idle);
+                }
+                else
+                {
+                    audioSource.PlayOneShot(data.emptyClip);
+                    if (autoReload)
+                        reloadingState = ReloadingState.StartReloading;
+                }
+            }
+            if (Input.GetButtonDown("Fire2"))
+            {
+                reloadingState = ReloadingState.StartReloading;
+            }            
+        }
+    }
+
+    protected virtual void Update()
+    {
+        Aim(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+    }
+}
